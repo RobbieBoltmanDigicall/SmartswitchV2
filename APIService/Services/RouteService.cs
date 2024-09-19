8c12272ce69dc727874f1611c39be75aac201550
Robbie Boltman <Robbie.Boltman@digicall.co.za>
2024-09-19 13:28:30 +0200

    Claims page

--- APIService/Services/RouteService.cs
+++ APIService/Services/RouteService.cs
@@ -1,4 +1,7 @@
-﻿using ClaimsService.Models.Repositories.Interfaces;
+﻿using ClaimsService.Models;
+using ClaimsService.Models.Repositories.Interfaces;
+using Newtonsoft.Json;
+using System.Text;
 using Route = ClaimsService.DAL.Route;
 
 namespace ClaimsService.Services
@@ -24,5 +27,25 @@ namespace ClaimsService.Services
 
         public List<Route> GetRoutesForClient(int clientId) =>
             _routeRepository.GetAllRoutesForClient(clientId);
+
+        public async Task<bool> CallRoute(RequestModel request)
+        {
+            //var _httpClient = new HttpClient();
+
+            //var jsonPayload = JsonConvert.SerializeObject(request.ClaimsBody);
+            //var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+            //// TODO: Add logging
+
+            //// TODO: Get client's Claim URL here
+            //var claimUrl = "";
+
+            //var response = await _httpClient.PostAsync(claimUrl, content);
+            //if (!response.IsSuccessStatusCode)
+            //{
+            //    // TODO: log failure in error table
+            //    throw new Exception($"Failed to send claim payload. Status code: {response.StatusCode}");
+            //}
+            return true;
+        }
     }
 }
