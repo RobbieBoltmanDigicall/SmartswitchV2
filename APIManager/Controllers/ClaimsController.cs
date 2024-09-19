36b82053731ee12aca71c7b0494ef59e34910715
Robbie Boltman <Robbie.Boltman@digicall.co.za>
2024-09-19 08:26:57 +0200

    Create API manager claims list page

---
+++ APIManager/Controllers/ClaimsController.cs
@@ -0,0 +1,43 @@
+﻿using APIManager.Models.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Route = APIManager.Models.Claims.Route;
+
+namespace APIManager.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ClaimsController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ClaimsList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5003/ManagerAPI/GetAllRoutes");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, "Error retrieving data from the external API.");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var viewModel = JsonConvert.DeserializeObject<List<Route>>(content);
+
+            return View("~/Views/Claims/ClaimsList.cshtml", viewModel);
+        }
+    }
+}
--- APIManager/Models/Claims/RouteViewModel.cs
+++ APIManager/Models/Claims/RouteViewModel.cs
@@ -2,79 +2,108 @@
 {
     public class Route
     {
-        public int Id { get; set; }
+        public int RouteId { get; set; }
+        public int RouteTypeId { get; set; }
         public RouteType RouteType { get; set; }
         public string RoutePath { get; set; }
         public int Order { get; set; }
-        public List<RouteHeader> RouteHeaders { get; set; }
-        public List<RouteParameter> RouteParameters { get; set; }
+        public ICollection<RouteHeader>? RouteHeaders { get; set; }
+        public ICollection<RouteParameter>? RouteParameters { get; set; }
+        public RouteBody? RouteBody { get; set; }
+        public int MethodTypeId { get; set; }
         public MethodType MethodType { get; set; }
-        public List<Client> Clients { get; set; }
+        public ICollection<Client> Clients { get; set; }
+        public Response Response { get; set; }
+    }
+
+    public class DataType
+    {
+        public int DataTypeId { get; set; }
+        public string DataTypeName { get; set; }
+    }
+
+    public class Response
+    {
+        public int ResponseId { get; set; }
+        public Route Route { get; set; }
+    }
+
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public ICollection<Route> Routes { get; set; }
+    }
+
+    public class MethodType
+    {
+        public int MethodTypeId { get; set; }
+        public string MethodTypeName { get; set; }
+        public Route Route { get; set; }
     }
 
     public class RouteType
     {
-        public int Id { get; set; }
+        public int RouteTypeId { get; set; }
         public string RouteTypeName { get; set; }
+        public Route Route { get; set; }
     }
 
     public class RouteHeader
     {
-        public int Id { get; set; }
+        public int RouteHeaderId { get; set; }
+        public int RouteId { get; set; }
+        public Route Route { get; set; }
         public string HeaderKey { get; set; }
         public string HeaderValue { get; set; }
+        public int DataTypeId { get; set; }
         public DataType DataType { get; set; }
     }
 
     public class RouteParameter
     {
-        public int Id { get; set; }
+        public int RouteParameterId { get; set; }
+        public int RouteId { get; set; }
+        public Route Route { get; set; }
         public string ParameterKey { get; set; }
         public string ParameterValue { get; set; }
+        public int DataTypeId { get; set; }
         public DataType DataType { get; set; }
     }
 
-    public class MethodType
+    public class RouteBody
     {
-        public int Id { get; set; }
-        public string MethodTypeName { get; set; }
+        public int RouteBodyId { get; set; }
+        public int RouteId { get; set; }
+        public Route Route { get; set; }
+        public int BodyTypeId { get; set; }
+        public BodyType BodyType { get; set; }
+        public int? ApplicationTypeId { get; set; }
+        public ApplicationType? ApplicationType { get; set; }
+        public ICollection<RouteBodyParameter> RouteBodyParameters { get; set; }
     }
 
-    public class Client
+    public class ApplicationType
     {
-        public int Id { get; set; }
-        public string ClientName { get; set; }
+        public int ApplicationTypeId { get; set; }
+        public string ApplicationTypeName { get; set; }
     }
 
-    public class DataType
+    public class RouteBodyParameter
     {
-        public int Id { get; set; }
-        public string DataTypeName { get; set; }
+        public int RouteBodyParameterId { get; set; }
+        public int RouteBodyId { get; set; }
+        public RouteBody RouteBody { get; set; }
+        public string BodyKey { get; set; }
+        public string BodyValue { get; set; }
+        public int DataTypeId { get; set; }
+        public DataType DataType { get; set; }
     }
 
-    public class RouteViewModel
+    public class BodyType
     {
-        public int Id { get; set; }
-        public string RouteType { get; set; }
-        public string RoutePath { get; set; }
-        public int Order { get; set; }
-        public List<RouteHeaderViewModel> RouteHeaders { get; set; }
-        public List<RouteParameterViewModel> RouteParameters { get; set; }
-        public string MethodType { get; set; }
-        public List<string> Clients { get; set; }
+        public int BodyTypeId { get; set; }
+        public string BodyTypeName { get; set; }
     }
 
-    public class RouteHeaderViewModel
-    {
-        public string HeaderKey { get; set; }
-        public string HeaderValue { get; set; }
-        public string DataTypeName { get; set; }
-    }
-
-    public class RouteParameterViewModel
-    {
-        public string ParameterKey { get; set; }
-        public string ParameterValue { get; set; }
-        public string DataTypeName { get; set; }
-    }
 }
